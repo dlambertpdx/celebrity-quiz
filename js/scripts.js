af5bd225606a5ae87de021ff2720d551f426301a
Donna Lambert <dlambertpdx@gmail.com>
2017-08-16 16:51:37 -0700

    revamp celebrity quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,90 @@
-$(function() {
-  $('form').submit(function(event) {
-    event.preventDefault();
-    var age = parseInt($("input#age").val());
-    var date = $("select#date").val();
-    var music = $("input:radio[name=music]:checked").val();
-    var match;
+// Backend Logic
+var choose = function(dine, movie) {
+  if(dine === 'pizza' && movie === 'drama') {
+    return 'img/carrot.jpg'
+  }
+  if(dine === 'molecule' && movie === 'comedy') {
+    return 'img/carrot.jpg'
+  }
+  if(dine === 'fast' && movie === 'horror') {
+    return 'img/carrot.jpg'
+  }
+  if(dine === 'homemade' && movie === 'artsy') {
+    return 'img/carrot.jpg'
+  }
 
-    if(date === "thrill") {
-      if(music === "rock") {
-        match = "white";
-      } else if(date === "vacation") {
-        if(music === "classical") {
-          match = "feldman";
-        } else if(date === "dine") {
-          if(music ==== "country") {
-            match = "seinfeld";
-          }
-        }
-      }
-    }
-$(".output").text(match);
+  if(dine === 'pizza' && movie === 'drama') {
+    return 'img/iggy.jpg'
+  }
+  if(dine === 'molecule' && movie === 'comedy') {
+    return 'img/iggy.jpg'
+  }
+  if(dine === 'fast' && movie === 'horror') {
+    return 'img/iggy.jpg'
+  }
+  if(dine === 'homemade' && movie === 'artsy') {
+    return 'img/iggy.jpg'
+  }
+
+  if(dine === 'pizza' && movie === 'drama') {
+    return 'img/steve.jpg'
+  }
+  if(dine === 'molecule' && movie === 'comedy') {
+    return 'img/steve.jpg'
+  }
+  if(dine === 'fast' && movie === 'horror') {
+    return 'img/steve.jpg'
+  }
+  if(dine === 'homemade' && movie === 'artsy') {
+    return 'img/steve.jpg'
+  }
+
+  if(dine === 'pizza' && movie === 'drama') {
+    return 'img/lindsay.jpg'
+  }
+  if(dine === 'molecule' && movie === 'comedy') {
+    return 'img/lindsay.jpg'
+  }
+  if(dine === 'fast' && movie === 'horror') {
+    return 'img/lindsay.jpg'
+  }
+  if(dine === 'homemade' && movie === 'artsy') {
+    return 'img/lindsay.jpg'
+  }
+
+  if(dine === 'pizza' && movie === 'drama') {
+    return 'img/linda.jpg'
+  }
+  if(dine === 'molecule' && movie === 'comedy') {
+    return 'img/linda.jpg'
+  }
+  if(dine === 'fast' && movie === 'horror') {
+    return 'img/linda.jpg'
+  }
+  if(dine === 'homemade' && movie === 'artsy') {
+    return 'img/linda.jpg'
+  }
+
+  if(dine === 'pizza' && movie === 'drama') {
+    return 'img/meryl.jpg'
+  }
+  if(dine === 'molecule' && movie === 'comedy') {
+    return 'img/meryl.jpg'
+  }
+  if(dine === 'fast' && movie === 'horror') {
+    return 'img/meryl.jpg'
+  }
+  if(dine === 'homemade' && movie === 'artsy') {
+    return 'img/meryl.jpg'
+  }
+}
+
+// front-end Logic
+$(document).ready(function() {
+  $('input#tpUnder').click(function() {
+    window.location = "img/tp.jpg"
+  });
+  $('input#tpOnTop').click(function() {
+    window.location = "img/tp.jpg"
   });
 });
